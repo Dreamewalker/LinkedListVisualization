b32214af83cdda9230683263bd87fdfb23385ade
MaxMorning <hml0814@163.com>
2021-07-28 20:31:54 +0800

    UI初步设计完成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +22,126 @@ namespace LinkedListVisualization
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 窗口拖动缩放相关
+        public const int WM_SYSCOMMAND = 0x112;
+        public HwndSource HwndSource;
+
+        public enum ResizeDirection
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8,
+        }
+
+        public Dictionary<ResizeDirection, Cursor> cursors = new Dictionary<ResizeDirection, Cursor>
+            {
+            {ResizeDirection.Top, Cursors.SizeNS},
+            {ResizeDirection.Bottom, Cursors.SizeNS},
+            {ResizeDirection.Left, Cursors.SizeWE},
+            {ResizeDirection.Right, Cursors.SizeWE},
+            {ResizeDirection.TopLeft, Cursors.SizeNWSE},
+            {ResizeDirection.BottomRight, Cursors.SizeNWSE},
+            {ResizeDirection.TopRight, Cursors.SizeNESW},
+            {ResizeDirection.BottomLeft, Cursors.SizeNESW}
+            };
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
         public MainWindow()
         {
             InitializeComponent();
+            this.SourceInitialized += MainWindow_SourceInitialized;
+            this.Loaded += MainWindow_Loaded;
+            this.MouseMove += MainWindow_MouseMove;
+        }
+
+        void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                FrameworkElement element = e.OriginalSource as FrameworkElement;
+                if (element != null && !element.Name.Contains("Resize"))
+                {
+                    this.Cursor = Cursors.Arrow;
+                }
+            }
+        }
+
+        void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            this.HwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ControlTemplate customWindowTemplate = App.Current.Resources["CustomWindowTemplete"] as ControlTemplate;
+            if (customWindowTemplate != null)
+            {
+                var TopLeft = customWindowTemplate.FindName("ResizeTopLeft", this) as Rectangle;
+                TopLeft.MouseMove += ResizePressed;
+                TopLeft.MouseDown += ResizePressed;
+                var Top = customWindowTemplate.FindName("ResizeTop", this) as Rectangle;
+                Top.MouseMove += ResizePressed;
+                Top.MouseDown += ResizePressed;
+                var TopRight = customWindowTemplate.FindName("ResizeTopRight", this) as Rectangle;
+                TopRight.MouseMove += ResizePressed;
+                TopRight.MouseDown += ResizePressed;
+                var Left = customWindowTemplate.FindName("ResizeLeft", this) as Rectangle;
+                Left.MouseMove += ResizePressed;
+                Left.MouseDown += ResizePressed;
+                var Right = customWindowTemplate.FindName("ResizeRight", this) as Rectangle;
+                Right.MouseMove += ResizePressed;
+                Right.MouseDown += ResizePressed;
+                var BottomLeft = customWindowTemplate.FindName("ResizeBottomLeft", this) as Rectangle;
+                BottomLeft.MouseMove += ResizePressed;
+                BottomLeft.MouseDown += ResizePressed;
+                var Bottom = customWindowTemplate.FindName("ResizeBottom", this) as Rectangle;
+                Bottom.MouseMove += ResizePressed;
+                Bottom.MouseDown += ResizePressed;
+                var BottomRight = customWindowTemplate.FindName("ResizeBottomRight", this) as Rectangle;
+                BottomRight.MouseMove += ResizePressed;
+                BottomRight.MouseDown += ResizePressed;
+            }
+        }
+
+        public void ResizePressed(object sender, MouseEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            ResizeDirection direction = (ResizeDirection)Enum.Parse(typeof(ResizeDirection), element.Name.Replace("Resize", ""));
+            this.Cursor = cursors[direction];
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                ResizeWindow(direction);
+            }
+        }
+
+        public void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(HwndSource.Handle, WM_SYSCOMMAND, (IntPtr)(61440 + direction), IntPtr.Zero);
+        }
+
+        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
